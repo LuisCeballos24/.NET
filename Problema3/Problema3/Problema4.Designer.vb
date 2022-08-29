7dbe279fe5f8f0f171c5bb02fab99580e37e6ec1
Severiano Atencio <satencio@consultec-ti.com>
2022-08-28 19:41:45 -0500

    add: problema 2 y 4

---
+++ Problema3/Problema3/Problema4.Designer.vb
@@ -0,0 +1,114 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Problema4
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtResultado = New System.Windows.Forms.TextBox()
+        Me.txtOperación = New System.Windows.Forms.TextBox()
+        Me.btnEjecutar = New System.Windows.Forms.Button()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(148, 44)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(158, 41)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Resultado"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.Location = New System.Drawing.Point(143, 197)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(163, 41)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Operación"
+        '
+        'txtResultado
+        '
+        Me.txtResultado.Enabled = False
+        Me.txtResultado.Location = New System.Drawing.Point(158, 111)
+        Me.txtResultado.Name = "txtResultado"
+        Me.txtResultado.Size = New System.Drawing.Size(125, 27)
+        Me.txtResultado.TabIndex = 2
+        '
+        'txtOperación
+        '
+        Me.txtOperación.Enabled = False
+        Me.txtOperación.Location = New System.Drawing.Point(158, 267)
+        Me.txtOperación.Name = "txtOperación"
+        Me.txtOperación.Size = New System.Drawing.Size(125, 27)
+        Me.txtOperación.TabIndex = 3
+        '
+        'btnEjecutar
+        '
+        Me.btnEjecutar.Location = New System.Drawing.Point(142, 324)
+        Me.btnEjecutar.Name = "btnEjecutar"
+        Me.btnEjecutar.Size = New System.Drawing.Size(94, 29)
+        Me.btnEjecutar.TabIndex = 4
+        Me.btnEjecutar.Text = "Ejecutar"
+        Me.btnEjecutar.UseVisualStyleBackColor = True
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Panel1.Controls.Add(Me.btnEjecutar)
+        Me.Panel1.Location = New System.Drawing.Point(36, 12)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(396, 380)
+        Me.Panel1.TabIndex = 5
+        '
+        'Problema4
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(488, 427)
+        Me.Controls.Add(Me.txtOperación)
+        Me.Controls.Add(Me.txtResultado)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Problema4"
+        Me.Text = "Problema4"
+        Me.Panel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtResultado As TextBox
+    Friend WithEvents txtOperación As TextBox
+    Friend WithEvents btnEjecutar As Button
+    Friend WithEvents Panel1 As Panel
+End Class
---
+++ Problema3/Problema3/problema2.vb
@@ -0,0 +1,12 @@
+﻿Public Class Problema2
+    Private Sub btnCalcular_Click(sender As Object, e As EventArgs) Handles btnCalcular.Click
+        Dim edadJuan, edadAlberto, edadAna As Integer
+        edadJuan = InputBox("¿Cúal es la edad de Juan?")
+        edadAlberto = edadJuan * 0.66
+        edadAna = edadJuan * 1.33
+        txtEdadAna.Text = edadAna
+        txtEdadAlberto.Text = edadAlberto
+        txtEdadJuan.Text = edadJuan
+        txtEdadMama.Text = edadJuan + edadAna + edadAlberto
+    End Sub
+End Class

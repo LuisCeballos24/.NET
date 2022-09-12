11cf2fe87f3c4a2ae06432552fa25024b15dc03a
Severiano Atencio <satencio@consultec-ti.com>
2022-09-11 20:30:35 -0500

    add: problema 6

---
+++ Laboratorio2/Laboratorio2/Alcaldia.vb
@@ -0,0 +1,52 @@
+﻿Public Class Alcaldia
+    Private alcalde(3) As Integer
+
+    Public Sub cargarAlcaldes(ByVal alcalde1 As Integer, ByVal alcalde2 As Integer, ByVal alcalde3 As Integer, ByVal alcalde4 As Integer)
+        alcalde(0) = alcalde1
+        alcalde(1) = alcalde2
+        alcalde(2) = alcalde3
+        alcalde(3) = alcalde4
+    End Sub
+
+    Public Function calcularMayor() As String
+        Dim indice As Integer
+
+        For i = 0 To alcalde.Length - 1
+            If alcalde(i) = alcalde.Max Then
+                indice = i
+                Exit For
+            End If
+        Next
+
+        Return numeroDeAlcalde(indice)
+    End Function
+
+    Public Function calcularMenor() As String
+        Dim indice As Integer
+
+        For i = 0 To alcalde.Length - 1
+            If alcalde(i) = alcalde.Min Then
+                indice = i
+                Exit For
+            End If
+        Next
+
+        Return numeroDeAlcalde(indice)
+    End Function
+
+    Private Function numeroDeAlcalde(ByVal alcalde As Integer)
+        Select Case alcalde
+            Case 0
+                Return "Alcalde 1"
+            Case 1
+                Return "Alcalde 2"
+            Case 2
+                Return "Alcalde 3"
+            Case 3
+                Return "Alcalde 4"
+            Case Else
+                Return "Alcalde Incorrecto"
+        End Select
+    End Function
+
+End Class
--- Laboratorio2/Laboratorio2/frmProblema1.vb
+++ Laboratorio2/Laboratorio2/frmProblema1.vb
@@ -1,3 +1,35 @@
 ﻿Public Class frmProblema1
+    Dim less500 As New List(Of Integer)
+    Dim between As New List(Of Integer)
+    Dim more1000 As New List(Of Integer)
 
+    Private Sub btnAgregarVenta_Click(sender As Object, e As EventArgs) Handles btnAgregarVenta.Click
+        Dim venta As Integer
+        venta = Val(txtVenta.Text)
+        If venta < 500 Then
+            less500.Add(venta)
+        ElseIf venta >= 500 And venta <= 1000 Then
+            between.Add(venta)
+        ElseIf venta > 1000 Then
+            more1000.Add(venta)
+        End If
+        txtVenta.Clear()
+    End Sub
+
+    Private Sub btnTotal_Click(sender As Object, e As EventArgs) Handles btnTotal.Click
+        ' Calcular total Mayor a 1000
+        txtCuantas1000.Text = more1000.Count
+        txtCuanto1000.Text = more1000.Sum
+
+        ' Calcular total menores a 500
+        txtCuantasBetween.Text = between.Count
+        txtCuantoBetween.Text = between.Sum
+
+        ' Calcular total menores a 500
+        txtCuantasMenor500.Text = less500.Count
+        txtCuantoMenor500.Text = less500.Sum
+
+        ' Calcular total ventas
+        txtTotalVentas.Text = more1000.Sum + between.Sum + less500.Sum
+    End Sub
 End Class
